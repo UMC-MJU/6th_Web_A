d5773824d7137f50d0592e9ffd6fee9341c83905
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-04-06 20:17:46 +0900

    실습: 카운터의 DOM을 선택합니다.

---
+++ 테비-박민철/chapter1/practice/counter.js
@@ -0,0 +1,7 @@
+const counterStatus = document.querySelector("#counter-status");
+const plusButton = document.querySelector("#plus-button");
+const minusButton = document.querySelector("#minus-button");
+
+console.log(counterStatus, "counterStatus");
+console.log(plusButton, "plusButton");
+console.log(minusButton, "minusButton");
