e9f613086ca5f31d81791baab1733fed0213733f
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-04-06 20:21:16 +0900

    실습: 버튼에 이벤트를 설정합니다.

--- 테비-박민철/chapter1/practice/counter.js
+++ 테비-박민철/chapter1/practice/counter.js
@@ -2,6 +2,13 @@ const counterStatus = document.querySelector("#counter-status");
 const plusButton = document.querySelector("#plus-button");
 const minusButton = document.querySelector("#minus-button");
 
-console.log(counterStatus, "counterStatus");
-console.log(plusButton, "plusButton");
-console.log(minusButton, "minusButton");
+const handleClickPlusButton = () => {
+  console.log("plus-button이 클릭됨");
+};
+
+const handleClickMinusButton = () => {
+  console.log("minus-button이 클릭됨");
+};
+
+plusButton.addEventListener("click", handleClickPlusButton);
+minusButton.addEventListener("click", handleClickMinusButton);
