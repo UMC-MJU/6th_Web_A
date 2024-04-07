d28a57b041bb23d340ce7b72aaa4e1f261048c01
jeon99yu <jeongkyu512@gmail.com>
2024-04-07 17:54:32 +0900

    주석설명 추가

--- 릴규-이정규/Practices/counter/counter.js
+++ 릴규-이정규/Practices/counter/counter.js
@@ -3,11 +3,13 @@ const increaseButton = document.getElementById("increase");
 const decreaseButton = document.getElementById("decrease");
 
 function increase() {
+  // 10진수 사용해야 오류를 막을 수 있음
   let currentValue = parseInt(numberElement.textContent, 10);
   numberElement.textContent = currentValue + 1;
 }
 
 function decrease() {
+  // 10진수 사용해야 오류를 막을 수 있음
   let currentValue = parseInt(numberElement.textContent, 10);
   numberElement.textContent = currentValue - 1;
 }
