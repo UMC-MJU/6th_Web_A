e22b165e5d48535c41128bcee3246d5548e0963a
jeon99yu <jeongkyu512@gmail.com>
2024-04-07 17:00:16 +0900

    불필요한 함수제거

--- 릴규-이정규/Practices/counter/counter.js
+++ 릴규-이정규/Practices/counter/counter.js
@@ -1,18 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const numberElement = document.getElementById("number");
-  const increaseButton = document.getElementById("increase");
-  const decreaseButton = document.getElementById("decrease");
+const numberElement = document.getElementById("number");
+const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
 
-  function increase() {
-    let currentValue = parseInt(numberElement.textContent, 10);
-    numberElement.textContent = currentValue + 1;
-  }
+function increase() {
+  let currentValue = parseInt(numberElement.textContent, 10);
+  numberElement.textContent = currentValue + 1;
+}
 
-  function decrease() {
-    let currentValue = parseInt(numberElement.textContent, 10);
-    numberElement.textContent = currentValue - 1;
-  }
+function decrease() {
+  let currentValue = parseInt(numberElement.textContent, 10);
+  numberElement.textContent = currentValue - 1;
+}
 
-  increaseButton.addEventListener("click", increase);
-  decreaseButton.addEventListener("click", decrease);
-});
+increaseButton.addEventListener("click", increase);
+decreaseButton.addEventListener("click", decrease);
