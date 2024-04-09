180d0ce8edca63c4e8e360882f95163647aeeb5a
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-04-09 20:29:39 +0900

    미션1: Todo List 구현하기

--- 테비-박민철/chapter2/src/mission/todo-list/page.jsx
+++ 테비-박민철/chapter2/src/mission/todo-list/page.jsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
 import Wrapper from "../../component/wrapper";
 
 const TodoListPage = () => {
-  return <Wrapper>안녕하세요 Todo List입니다.</Wrapper>;
+  const [todoItems, setTodoItems] = useState([]);
+  const [completedItems, setCompletedItems] = useState([]);
+
+  const handleAddTodoItem = (text) => {
+    const newItem = {
+      id: Date.now(),
+      text: text.trim(),
+    };
+    setTodoItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  const handleComplete = (id) => {
+    const completedItem = todoItems.find((item) => item.id === id);
+    setCompletedItems((prevItems) => [...prevItems, completedItem]);
+    setTodoItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const handleDelete = (id, fromCompleted = false) => {
+    if (fromCompleted) {
+      setCompletedItems((prevItems) =>
+        prevItems.filter((item) => item.id !== id)
+      );
+    } else {
+      setTodoItems((prevItems) => prevItems.filter((item) => item.id !== id));
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.isComposing || event.keyCode === 229) return;
+    if (event.key === "Enter" && event.target.value.trim() !== "") {
+      handleAddTodoItem(event.target.value);
+      event.target.value = "";
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div style={{ display: "grid", placeContent: "center" }}>
+        <h1>UMC Study Plan</h1>
+      </div>
+      <hr />
+      <div
+        style={{
+          display: "grid",
+          placeContent: "center",
+        }}
+      >
+        <input
+          placeholder="UMC 스터디 계획을 작성해 보세요."
+          onKeyDown={handleKeyDown}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "5vw",
+            width: "40vw",
+          }}
+        >
+          <div>
+            <h3 style={{ borderBottom: "1px solid black" }}>해야할일</h3>
+            {todoItems.map((item) => (
+              <div key={item.id}>
+                {item.text}
+                <button onClick={() => handleComplete(item.id)}>완료</button>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h3 style={{ borderBottom: "1px solid black" }}>한일</h3>
+            {completedItems.map((item) => (
+              <div key={item.id}>
+                {item.text}
+                <button onClick={() => handleDelete(item.id, true)}>
+                  삭제
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default TodoListPage;
