8b7d3bf1dadbca3381d9220692a8ba826db9f3c7
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-04-29 23:19:41 +0900

    feat: main banner component

---
+++ 테비-박민철/chapter3/src/components/MainBanner.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Banner = styled.div`
+  height: 300px;
+  padding: 0 auto;
+  background-color: black;
+  display: grid;
+  place-content: center;
+  color: white;
+  width: 100%;
+`;
+
+const MainBanner = () => {
+  return (
+    <Banner>
+      <h2>환영합니다</h2>
+    </Banner>
+  );
+};
+
+export default MainBanner;
