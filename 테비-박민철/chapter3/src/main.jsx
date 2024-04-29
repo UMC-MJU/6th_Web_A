c8e39d4e0b60d5cbaaf2950ccf8c789877bd1660
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-04-29 23:21:07 +0900

    feat: root component

--- 테비-박민철/chapter3/src/main.jsx
+++ 테비-박민철/chapter3/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToggleProvider } from "./utils/contexts/toggleContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ToggleProvider>
+        <App />
+      </ToggleProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
