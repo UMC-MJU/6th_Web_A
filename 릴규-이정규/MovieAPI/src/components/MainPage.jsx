ce42d7c7087cbd306e46b3a73c00a267df34c8f0
jeon99yu <jeongkyu512@gmail.com>
2024-05-04 15:29:09 +0900

    moviePage구현

---
+++ 릴규-이정규/MovieAPI/src/components/MainPage.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const MainPageContainer = styled.div`
+  background-color: rgba(15, 9, 59, 0.856);
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`;
+
+const WelcomeMessage = styled.h2`
+  color: #fff;
+`;
+
+const LowerSection = styled.div`
+  width: 100%;
+  padding: 50px;
+  box-sizing: border-box;
+  text-align: center;
+  background-color: rgba(15, 9, 59, 0.856);
+`;
+
+const Text = styled.p`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const SearchInput = styled.input`
+  padding: 10px;
+  margin-top: 10px;
+  width: 40%;
+  border: 1px solid #333;
+  border-radius: 50px;
+`;
+
+function MainPage() {
+  return (
+    <div>
+      <GlobalStyle />
+      <MainPageContainer>
+        <WelcomeMessage>환영합니다!</WelcomeMessage>
+      </MainPageContainer>
+      <LowerSection>
+        <Text> 🎥 Find your movies !</Text>
+        <SearchInput type="text" />
+      </LowerSection>
+    </div>
+  );
+}
+
+export default MainPage;
