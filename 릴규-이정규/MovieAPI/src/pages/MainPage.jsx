c293884d3f93b7126a08fbbdcb45c0537b5e66a2
jeon99yu <jeongkyu512@gmail.com>
2024-05-19 20:24:06 +0900

    tmdb 검색

--- 릴규-이정규/MovieAPI/src/pages/MainPage.jsx
+++ 릴규-이정규/MovieAPI/src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import axios from "axios";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    font-family: Arial, sans-serif;
+    background-color: rgba(15, 9, 59, 0.856);
   }
 `;
 
@@ -36,15 +39,68 @@ const Text = styled.p`
   font-weight: bold;
 `;
 
+const SearchWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 10px;
+`;
+
 const SearchInput = styled.input`
   padding: 10px;
-  margin-top: 10px;
   width: 40%;
   border: 1px solid #333;
   border-radius: 50px;
 `;
 
+const SearchButton = styled.button`
+  width: 50px;
+  height: 42px;
+  border: none;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: rgb(220, 194, 99);
+`;
+
+const SearchResultContainer = styled.div`
+  color: white;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Movie = styled.div`
+  margin: 5px;
+  padding: 10px;
+  width: 90%;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+`;
+
 function MainPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    if (!searchTerm) return;
+    const apiKey = "8e2d1e6d3637d6fb007d0df41e9c5ff5";
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+      searchTerm
+    )}`;
+
+    try {
+      const response = await axios.get(url);
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
   return (
     <div>
       <GlobalStyle />
@@ -53,7 +109,22 @@ function MainPage() {
       </MainPageContainer>
       <LowerSection>
         <Text> 🎥 Find your movies !</Text>
-        <SearchInput type="text" />
+        <SearchWrapper>
+          <SearchInput
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <SearchButton onClick={fetchMovies}>🔍</SearchButton>
+        </SearchWrapper>
+        <SearchResultContainer>
+          {movies.map((movie) => (
+            <Movie key={movie.id}>
+              <strong>{movie.title}</strong> (
+              {movie.release_date && movie.release_date.split("-")[0]})
+            </Movie>
+          ))}
+        </SearchResultContainer>
       </LowerSection>
     </div>
   );
