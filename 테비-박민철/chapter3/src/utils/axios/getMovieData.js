a7f28277beebbca9356ce41c9b5f8c07b6d72ce3
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-05-24 09:47:17 +0900

    chore: api key env 파일에 넣어 gitignore 처리

--- 테비-박민철/chapter3/src/utils/axios/getMovieData.js
+++ 테비-박민철/chapter3/src/utils/axios/getMovieData.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const VITE_APP_MOVIE_API_KEY = "8e2d1e6d3637d6fb007d0df41e9c5ff5";
-
 const getMovieData = async (type) => {
   const data = await axios.get(
-    `https://api.themoviedb.org/3/movie/${type}?api_key=${VITE_APP_MOVIE_API_KEY}`
+    `https://api.themoviedb.org/3/movie/${type}?api_key=${
+      import.meta.env.VITE_APP_MOVIE_API_KEY
+    }`
   );
 
   return data;
