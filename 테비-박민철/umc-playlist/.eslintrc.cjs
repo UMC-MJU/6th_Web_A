5b60960e889227f38151db289f70a25ca39d10c1
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-07-01 21:45:47 +0900

    docs: 초기 개발 환경 세팅

---
+++ 테비-박민철/umc-playlist/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }], // Allow JSX in .tsx files
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import/no-unresolved": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react/react-in-jsx-scope": "off", // React 17+ JSX Transform
+    "react/jsx-uses-react": "off", // React 17+ JSX Transform
+    "react/no-absolute-path": "off",
+  },
+};
